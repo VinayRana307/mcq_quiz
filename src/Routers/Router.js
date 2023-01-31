bf8f4447c80ffc3d8bc6de2a531f91d4f15ca980
vinay kumar <vinay6365355@gmail.com>
2023-01-31 17:20:01 +0530

    today commit

--- src/Routers/Router.js
+++ src/Routers/Router.js
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import WordQuiz from "../Components/Pages/MS_Word/MS_Word";
 import HomePage from "../Components/Pages/HomePage/HomePage";
+import { HashRouter } from 'react-router-dom';
 
 export default function Router() {
   return (
+    <HashRouter>
       <Routes>
             <Route path="/msword" element={<WordQuiz />} />
             <Route path="/home" element={<HomePage />} />
+            <Route path="/" element={<HomePage />} />
       </Routes>
+      </HashRouter>
   );
 }
